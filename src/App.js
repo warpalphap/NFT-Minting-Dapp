b725c1129c2b640c45a88d92eb24d8f5541f7ec7
warpalphap <85257861+warpalphap@users.noreply.github.com>
2022-06-25 18:44:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -163,8 +163,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 10) {
-      newMintAmount = 10;
+    if (newMintAmount > 20) {
+      newMintAmount = 20;
     }
     setMintAmount(newMintAmount);
   };
